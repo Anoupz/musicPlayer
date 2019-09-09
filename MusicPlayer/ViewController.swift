ebe3ffc73f08f722c8e88c9adf882dc9cea183df
Anoop N <anoupz@gmail.com>
2019-09-09 10:33:35 -0700

    Initial Commit

---
+++ MusicPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by Anoop Paravathmadam on 9/9/19.
+//  Copyright © 2019 Anoop Paravathmadam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
